8f658dc8d1cb431384eca1c17ad250c1e9c12809
Fernius07yt <59263688+Fernius07yt@users.noreply.github.com>
2024-05-28 23:37:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ function handleFile(event) {
     Papa.parse(file, {
         complete: function(results) {
             generateTable(results.data);
-            plotMap(results.data);
         }
     });
 }
@@ -41,39 +40,4 @@ function generateTable(data) {
 
     // Inicializar DataTables
     $('#csvTable').DataTable();
-}
-
-function plotMap(data) {
-    // Find the index of the Position column
-    const headerRow = data[0];
-    const positionIndex = headerRow.indexOf("Position");
-
-    if (positionIndex === -1) return; // If there's no "Position" column, do nothing
-
-    // Initialize the map
-    const map = L.map('map').setView([0, 0], 2); // Set to global view initially
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 18,
-    }).addTo(map);
-
-    const coordinates = [];
-
-    // Extract coordinates from the data
-    data.slice(1).forEach(row => {
-        const position = row[positionIndex];
-        if (position) {
-            const [lat, lng] = position.split(',').map(Number);
-            coordinates.push([lat, lng]);
-        }
-    });
-
-    if (coordinates.length === 0) return; // No valid coordinates
-
-    // Add markers and path to the map
-    const latLngs = coordinates.map(coord => L.latLng(coord[0], coord[1]));
-    L.polyline(latLngs, { color: 'blue' }).addTo(map);
-    latLngs.forEach(latlng => L.marker(latlng).addTo(map));
-
-    // Fit the map to the bounds of the coordinates
-    map.fitBounds(latLngs);
 }
